e3484a8dcdb0acfc160128b21d500d1921102274
Adam Streck <adam.streck@gmail.com>
2022-02-10 16:28:38 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pyfish",
+    version="1.0.0",
+    author="Adam Streck, Tom Kaufmann",
+    author_email="adam.streck@mdc-berlin.de",
+    description="Plotting tool for evolutionary population dynamics. Creates a Fish (Muller) plot.",
+    long_description=read('README'),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    keywords="plot genomics visualization",
+    packages=['pyfish', 'tests'],
+    install_requires=[
+        'numpy>=1.0',
+        'pandas>=1.0',
+        'scipy>=1.0',
+        'matplotlib>=3.0'
+    ],
+)
