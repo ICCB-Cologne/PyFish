0010d65a044394146b9307327c2e644091fafe37
Adam_Streck <adam.streck@gmail.com>
2022-02-11 15:05:37 +0100

    seed is now fixed

--- tests/test_pyfish.py
+++ tests/test_pyfish.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from pyfish.core import fish_plot, process_data
+from src.pyfish.core import fish_plot, process_data
 
 
 def test_pyfish():
     populations = np.array(
         [[0, 0, 100], [0, 1, 40], [0, 2, 20], [0, 3, 0], [1, 0, 10], [1, 3, 50],
          [1, 5, 100], [2, 4, 20], [2, 5, 50], [3, 0, 10], [3, 1, 20], [3, 5, 10]])
-
     parent_tree = np.array([[0, 1], [1, 2], [0, 3]])
-
     populations_df = pd.DataFrame(populations, columns=["Id", "Step", "Pop"])
     parent_tree_df = pd.DataFrame(parent_tree, columns=["ParentId", "ChildId"])
-
-    fish_plot(*process_data(populations_df, parent_tree_df, interpolation=2, seed=42))
+    data = process_data(populations_df, parent_tree_df, interpolation=1)
+    fish_plot(*data)
     plt.show()
 
 
